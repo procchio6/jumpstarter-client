faea4edfc8a83deb95b127042a33012c239665e5
Pat Rocchio <procchio6@gmail.com>
2017-08-24 14:49:09 -0400

    Display campaign with read only editor

--- src/components/CampaignPanel.js
+++ src/components/CampaignPanel.js
@@ -1,23 +1,29 @@
 import React, { Component } from 'react'
 import { Segment, Dimmer, Loader } from 'semantic-ui-react'
-import { stateToHTML } from 'draft-js-export-html'
-import { convertFromRaw } from 'draft-js'
-import SafeHTML from 'react-safe-html'
+import { EditorState, convertFromRaw } from 'draft-js'
+import Editor from 'draft-js-editor'
 
 class CampaignPanel extends Component {
 
   parsedContent = () => JSON.parse(this.props.content)
 
   render() {
-    const campaignHTML = this.props.content ?
-      stateToHTML(convertFromRaw(this.parsedContent())) : '<p> No campaign content! </p>'
+    const editorState = this.props.content ?
+      EditorState.createWithContent(convertFromRaw(this.parsedContent())) : EditorState.createEmpty()
 
     return (
       <Dimmer.Dimmable as={Segment} dimmed={this.props.loading} attached='bottom' padded='very'>
+
         <Dimmer active={this.props.loading} inverted>
           <Loader />
         </Dimmer>
-        <SafeHTML html={campaignHTML} />
+        
+        <Editor
+          placeholder="No campaign content!"
+          editorState={editorState}
+          readOnly
+        />
+
       </Dimmer.Dimmable>
     )
   }
